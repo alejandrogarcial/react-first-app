f23aac778bc92a87199cc88099d13c92d784770a
Alex Garcia <ingalexgalo@gmail.com>
2020-02-19 22:20:04 -0600

    build

---
+++ build/precache-manifest.1e6da5298b5b6bdacecd3274d01bcbc8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384d129f13034a6e2155815377113cf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e824e6d9dbd1c2297f1",
+    "url": "/static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "5718a807a581ece856d9",
+    "url": "/static/css/main.6b685295.chunk.css"
+  },
+  {
+    "revision": "2e824e6d9dbd1c2297f1",
+    "url": "/static/js/2.b7ed356a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.b7ed356a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5718a807a581ece856d9",
+    "url": "/static/js/main.f0c67a9a.chunk.js"
+  },
+  {
+    "revision": "f483d1f7cd20d8e3322f",
+    "url": "/static/js/runtime-main.70302f69.js"
+  },
+  {
+    "revision": "6bf9b4da7da578b9d3447adc363d32e1",
+    "url": "/static/media/bulma-logo.6bf9b4da.png"
+  }
+]);
